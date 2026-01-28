689a48f812907e7e5b5ecee8ecf9a1430542edaf
MukeshDalai <mukeshdalai.2307@gmail.com>
2026-01-28 20:25:22 +0530

    refactor: used tech-stack-icons for icons

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,51 +1,46 @@
 import React from 'react'
 import StackIcon from "tech-stack-icons";
-import html from '/HTML5.svg';
-import css from '/CSS3.svg';
-import js from '/JavaScript.svg';
-import py from '/Python.svg';
-import mySql from '/MySQL.svg';
-import java from '/Java.svg';
-
-import pbi from '/power-bi-icon.svg';
-import xl from '/microsoft-excel-icon.svg';
-import tbl from '/Tableau.svg';
-import pl from '/Ploty.svg';
-
-import pds from '/Pandas.svg';
-import npy from '/NumPy.svg';
-import se from '/Selenium.svg';
-import spy from '/Apache Spark.svg';
 
 import win from '/Windows 11.svg';
 import lx from '/Linux.svg';
 
 const row1 = [
-  { svg: html },
-  { svg: css },
-  { svg: js },
-  { svg: py },
-  { svg: mySql },
-  { svg: java },
+  { name: "js" },
+  { name: "typescript" },
+  { name: "html5" },
+  { name: "css3" },
+  { name: "python" },
+  { name: "java" },
 ]
 
 const row2 = [
-  { svg: pbi },
-  { svg: xl },
-  { svg: tbl },
-  { svg: pl },
+  { name: "react" },
+  { name: "nodejs" },
+  { name: "express" },
+  { name: "bootstrap5" },
+  { name: "tailwindcss" },
+  { name: "framer", variant: "dark" },
 ]
 
 const row3 = [
-  { svg: pds },
-  { svg: npy },
-  { svg: se },
-  { svg: spy },
+  { name: "mongodb" },
+  { name: "mysql" },
+  { name: "postgresql" },
+  { name: "mongoose" },
 ]
 
 const row4 = [
-  { svg: win },
-  { svg: lx },
+  { name: "git" },
+  { name: "github", variant: "dark"},
+  { name: "gitlab" },
+  { name: "figma" },
+  { name: "postman" },
+  { name: "aws" },
+]
+
+const row5 = [
+  // { name: "git" },
+  { name: "linux"},
 ]
 
 const TechStack = () => {
@@ -56,6 +51,7 @@ const TechStack = () => {
         <TechStackItems techList={row2} />
         <TechStackItems techList={row3} />
         <TechStackItems techList={row4} />
+        <TechStackItems techList={row5} />
       </div>
     </>
   )
@@ -67,7 +63,7 @@ const TechStackItems = ({ techList }) => {
       {
         techList?.map((icon, index) => {
           return (
-            <img src={icon.svg} key={index} alt="tech-stack-icon" className='size-16' />
+            <StackIcon key={index} className='size-16' {...icon}/>
           )
         })
       }
